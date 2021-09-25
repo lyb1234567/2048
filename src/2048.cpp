df2b1b9a955ee25f1d0350457b4adf6a2e35ba6b
Dixon <yliu431@sheffield.ac.uk>
2021-09-25 11:09:16 +0100

    fix src

---
+++ src/2048.cpp
@@ -0,0 +1,18 @@
+﻿#include "game board.h"
+#include"Number.h"
+#include<iostream>
+#include<Windows.h>
+#include<ctime>
+#include<conio.h>
+#include<fstream>
+#include<vector>
+#include"list.h"
+#include"Menu.h"
+#include"used_function.h"
+using namespace std;
+int main()
+{
+	Menu m;
+	m.start();
+	return 0;
+}
