99fc06052138a0e5bd3d9a9d214f3fad6cb79a95
Dixon <yliu431@sheffield.ac.uk>
2021-09-25 11:00:28 +0100

    create 2048

---
+++ header/Menu.h
@@ -0,0 +1,24 @@
+﻿#ifndef MENU_H_
+#define MEUN_H_
+#include "game board.h"
+#include"Number.h"
+#include<iostream>
+#include<Windows.h>
+#include<ctime>
+#include<conio.h>
+#include<fstream>
+#include<vector>
+#include"list.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class Menu
+{
+public:
+	Menu();
+	void start();
+    string AsciiArt2048();
+};
+#endif // !1
+
+#pragma once
